15085d15968c140db2ab45fd4f511d3c52e8cb6b
Yasmin <yasmin@lit.ifce.edu.br>
2020-03-06 09:33:35 -0300

    #5 doing the base of request

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Planets;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use GuzzleHttp\Client;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function requestApi()
+    {
+        $page = 1;
+        $responseJson = $this->makeRequestToStarWarsApi($page);
+        // dd($responseJson['next']);
+    }
+
+    public function makeRequestToStarWarsApi($page)
+    {
+        $client = new Client();
+        $response = $client->request('GET', ENV('API_STAR_WARS'). '?page=' . $page);
+
+        $responseJson = json_decode($response->getBody(), true);
+        return $responseJson;
+    }
 }
