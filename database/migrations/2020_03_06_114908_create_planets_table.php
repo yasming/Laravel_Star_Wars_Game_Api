0f246f1b3de0ddf40a9089399de845dab1391494
Yasmin <yasmin@lit.ifce.edu.br>
2020-03-06 09:01:42 -0300

    #2 creating tables

---
+++ database/migrations/2020_03_06_114908_create_planets_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planets', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('name');
+            $table->integer('rotations_period');
+            $table->integer('orbital_period');
+            $table->integer('diameter');
+            $table->string('climate');
+            $table->string('gravity');
+            $table->string('terrain');
+            $table->integer('surface_water');
+            $table->bigInteger('population');
+            $table->string('residents');
+            $table->string('films');
+            $table->string('created');
+            $table->string('edited');
+            $table->string('url');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planets');
+    }
+}
