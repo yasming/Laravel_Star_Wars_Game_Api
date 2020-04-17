fccdad558e89c0d0f07ed03ae5f7cba14158dc5d
Yasmin <yasmin@lit.ifce.edu.br>
2020-04-17 08:26:29 -0300

    json response in handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +51,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof \Illuminate\Validation\ValidationException) { return new JsonResponse(['errors' => $exception->errors()], 422); }
         return parent::render($request, $exception);
     }
 }
